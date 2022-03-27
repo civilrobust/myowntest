acaff64a7720c5fa41e8e98b706f67650d8f6de1
civilrobust <civil.robust@yahoo.com>
2022-03-27 15:59:56 +0100

    init1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/crobust/myniceprogram2
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gopherguides/greet"
+)
+
+func main() {
+	greet.Hello()
+
+	fmt.Println(greet.Shark)
+
+}
